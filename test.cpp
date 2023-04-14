9c22a2051ec672790aacfaf62896c01055b5a091
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-04-15 00:15:35 +0530

    updated

--- test.cpp
+++ test.cpp
@@ -3,25 +3,16 @@
 
 using namespace std;
 
-vector<int> rotateLeft(int, vector<int>);
-
+long arrayManipulation(int, vector<vector<int>>);
 int main(){
-    int x = 4;
-    vector<int> arr = {1,2,3,4,5};
-    vector<int> t = rotateLeft(x,arr);
-    for (int i = 0; i < 5; i++)
-    {
-        cout<<t[i]<<" ";
-    }
+    int x = 5;
+    vector<vector<int>> q = {
+        {1,2,100},
+        {2,5,100},
+        {3,4,100}
+    };
 }
 
-vector<int> rotateLeft(int d, vector<int> arr){
-    int temp;
-    for (int i = 0; i < d; i++)
-    {
-        temp = arr[0];
-        arr.erase(arr.begin());
-        arr.push_back(temp);
-    }
-    return arr;
+long arrayManipulation(int n, vector<vector<int>> queries){
+
 }
