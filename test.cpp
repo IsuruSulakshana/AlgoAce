d2446e17117bf474ad402cae6aca1c35d93c34c2
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-04-14 18:09:29 +0530

    done leftRotate

--- test.cpp
+++ test.cpp
@@ -3,11 +3,25 @@
 
 using namespace std;
 
-vector<int> dynamicArray(int, vector<vector<int>>);
-int main(){
+vector<int> rotateLeft(int, vector<int>);
 
+int main(){
+    int x = 4;
+    vector<int> arr = {1,2,3,4,5};
+    vector<int> t = rotateLeft(x,arr);
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<t[i]<<" ";
+    }
 }
 
-vector<int> dynamicArray(int n, vector<vector<int>> queries) {
-
+vector<int> rotateLeft(int d, vector<int> arr){
+    int temp;
+    for (int i = 0; i < d; i++)
+    {
+        temp = arr[0];
+        arr.erase(arr.begin());
+        arr.push_back(temp);
+    }
+    return arr;
 }
