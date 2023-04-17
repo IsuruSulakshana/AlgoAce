65a4afe100e14b1cc8d01e04223ea909f9985d53
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-04-18 01:43:09 +0530

    done diagonal difference

--- test.cpp
+++ test.cpp
@@ -1,29 +1,46 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include<vector>
 
 using namespace std;
-
-long arrayManipulation(int, vector<vector<int>>);
+void diagonalDifference(vector<vector<int>>); 
 int main(){
-    int x = 5;
     vector<vector<int>> q = {
-        {1,2,100},
-        {2,5,100},
-        {3,4,100}
+        {11,2,4},
+        {4,5,6},
+        {10,8,-12}
     };
-    long t = arrayManipulation(x, q);
-    cout<<t;
+    diagonalDifference(q);
 }
 
-long arrayManipulation(int n, vector<vector<int>> queries) {
-    vector<long> temp(n+1, 0);
-    for (int i = 0; i < queries.size(); i++) {
-        int a = queries[i][0];
-        int b = queries[i][1];
-        int k = queries[i][2];
-        temp[a] += k;
-        if (b+1 <= n) temp[b+1] -= k;
+void diagonalDifference(vector<vector<int>> arr) {
+    int pD = 0;
+    int sD = 0;
+    int n = arr.size();
+    int x = n/2;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                if (i == n/2 && n%2 == 1)
+                {
+                    sD = sD + arr[i][j];
+                    pD = pD + arr[i][j];
+                }
+                else
+                {
+                    pD = pD + arr[i][j];
+                }
+            }
+            else if (i == arr.size()-j)
+            {
+                sD = sD + arr[i][j];
+            }        
+        }
     }
-    long max = *max_element(temp.begin(), temp.end());
-    return max;
+    cout<<pD<<endl;
+    cout<<sD<<endl;
+    // int dif = abs(pD-sD);
+    // return dif;
 }
